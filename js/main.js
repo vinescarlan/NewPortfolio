06ef2bfe5b4a7a8ea6a1153156dd4f0a6a5eda50
vinescarlan <vinescarlan@gmail.com>
2015-10-10 12:36:39 +0800

    Finish off-canvas-menu functionalities

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 $(document).foundation();
+
+// Off-canvas menu toggle function
+(function () {
+	function toggleNav() {
+		var nav = document.querySelector("nav");
+		if (nav.className === "") {
+			nav.className = "menu-visible";
+		} else {
+			nav.className = "";
+		}
+	}
+
+	var menuIcon = document.getElementById("hamburger");
+	menuIcon.addEventListener("click", toggleNav);
+})();
