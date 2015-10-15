7712603e04257343a8dff05aa52ead684adf1fd1
vinescarlan <vinescarlan@gmail.com>
2015-10-15 15:34:50 +0800

    Create AJAX request

--- js/main.js
+++ js/main.js
@@ -17,3 +17,16 @@ $(document).foundation();
 	var contact = document.querySelector(".nav-item [href='info.html#contact']");
 	contact.onclick = toggleNav;
 })();
+
+// AJAX (project_details.json)
+var xhr = new XMLHttpRequest(),
+	projectItems; // Will store the contents of JSON file
+
+xhr.onreadystatechange = function () {
+	if (xhr.readyState == 4 && xhr.status == 200) {
+		projectItems = JSON.parse(xhr.responseText);
+	}
+}
+
+xhr.open("GET", "js/projects_details.json", true);
+xhr.send(null);
